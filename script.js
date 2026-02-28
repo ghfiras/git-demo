e1f3e033373a81c62e33884094ce14a27cd77460
Developer 2 <dev2@example.com>
2026-02-27 19:14:30 -0500

    Dev 2: Added reset button

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const counterDisplay = document.querySelector('.counter-value');
+const resetBtn = document.querySelector('.reset-btn');
 let count = 0;
 window.addEventListener('click', () => {
     count++;
     counterDisplay.textContent = count;  
     console.log("Current count: " + count);
+});
+
+resetBtn.addEventListener('click', (event) => {
+    event.stopPropagation(); 
+    count = 0;
+    counterDisplay.textContent = count;
 });
